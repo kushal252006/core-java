07663b7775822ce8429eedaae9dcd6df8f0b9c72
Kushalgowda <kushalkushalgowdahv@gmail.com>
2026-04-05 14:29:09 +0530

    perform

---
+++ Kushal.java
@@ -0,0 +1,20 @@
+class Kushal{
+
+static void getage(int age){
+System.out.println("age:"+age);
+}
+static int setscore(int score){
+System.out.println("score:"+score);
+return score;
+}
+static void addAdress(){
+System.out.println("bhosuru");
+}
+static int now(){
+int salary=10000;
+System.out.println(salary);
+return salary;
+
+
+}
+}
